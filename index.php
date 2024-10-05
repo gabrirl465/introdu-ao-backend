03a1054e9dad900415551104d994cd92f6363030
gabrirl465 <gvieiradasilva441@gmail.com>
2024-10-05 14:03:05 +0000

    adiconar a primeira pagina php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=], initial-scale=1.0">
+    <title>php pode entrar e ola mundo!</title>
+</head>
+<body>
+<?php
+$numero1=25
+$numero2=10
+$soma=$numero1+$numero2
+
+?>
+<p>a soma de 25 e 10 e de <?php echo $soma ?></p>
+
+</body>
+</html>
